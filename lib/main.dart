8bcaa0b02390f41437b1985e72e5087797ee461e
charan4112 <charanpendyala4112@gmail.com>
2025-02-06 16:48:57 -0500

    Step 2: Added image toggle feature with animation

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Hides debug banner
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CounterScreen(), // Calls the Counter Screen
+      home: CounterScreen(),
     );
   }
 }
@@ -24,11 +24,18 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
-  int _counter = 0; // Counter variable
+  int _counter = 0;
+  bool _showFirstImage = true; // Boolean to track image state
 
   void _incrementCounter() {
     setState(() {
-      _counter++; // Increase counter by 1
+      _counter++;
+    });
+  }
+
+  void _toggleImage() {
+    setState(() {
+      _showFirstImage = !_showFirstImage; // Toggle between images
     });
   }
 
@@ -38,28 +45,52 @@ class _CounterScreenState extends State<CounterScreen> {
       appBar: AppBar(
         title: Text('Flutter Demo Home Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-              style: TextStyle(fontSize: 16),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'You have pushed the button this many times:',
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(height: 10),
+          Text(
+            '$_counter',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+
+          // Animated Image Toggle
+          AnimatedOpacity(
+            duration: Duration(milliseconds: 500), // Smooth transition
+            opacity: 1.0,
+            child: Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    _showFirstImage ? 'assets/image1.jpg' : 'assets/image2.jpg',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            SizedBox(height: 10),
-            Text(
-              '$_counter', // Display counter value
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+          ),
+
+          SizedBox(height: 20),
+
+          // Toggle Image Button
+          ElevatedButton(
+            onPressed: _toggleImage,
+            child: Text("Toggle Image"),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter, // Calls _incrementCounter() on press
+        onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add), // + Symbol in FAB
+        child: Icon(Icons.add),
       ),
     );
   }
 }
-
